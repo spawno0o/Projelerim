bb844d7d77d2719a09426853d4dc6669ffc0f8b9
spawno0o <126720640+spawno0o@users.noreply.github.com>
2023-03-03 00:30:20 +0300

    Add files via upload

---
+++ notortalamasi/src/notortalamasi.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class notortalamasi {
+    public static void main(String[] args) {
+        int mat,fizik,kimya,turkce,tarih,muzik;
+
+        Scanner input=new Scanner(System.in);
+        System.out.print("matematik notunuzu giriniz");
+        mat= input.nextInt();
+
+        System.out.println("fizik notunuz girin");
+        fizik=input.nextInt();
+
+        System.out.println("kimya notunuzu girin");
+        kimya= input.nextInt();
+
+        System.out.println("turkce notunuzu girin");
+        turkce= input.nextInt();
+
+        System.out.println("tarih notunuzu girin");
+        tarih= input.nextInt();
+
+        System.out.println("muzik notunuzu girin");
+        muzik= input.nextInt();
+
+        int ortalama = ((mat + fizik+kimya+turkce+tarih+muzik)/6);
+        System.out.println("Ortalamanız : " + ortalama);
+
+        boolean a= ortalama>=60;
+        String burak = a? "geçti":"kaldı";
+        System.out.println(burak);
+
+    }
+}
