29c914ebb1b0a9eb9360df74fafc7a524ac2aa4a
spawno0o <126720640+spawno0o@users.noreply.github.com>
2023-03-27 23:00:03 +0300

    Add files via upload

---
+++ cift/src/cift.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+public class cift {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+/*        int i,x;
+        System.out.print("Sayı Giriniz : ");    for döngüsü ile
+        x=input.nextInt();
+        for(i=1;i<=x;i++){
+            if(i%2==0){
+                System.out.println(i);
+            }
+        }
+*/
+/* int x,i=1;
+        System.out.print("Sayı Giriniz : ");
+        x= input.nextInt();
+        while(i<=x){                while döngüsü ile
+            if(i%2==0){
+                System.out.println(i);
+            }
+            i++;
+        }
+*/
+
+/*        int i,x,say=0,toplam=0;
+        System.out.println("Sayı Giriniz ;");
+        x= input.nextInt();
+        for(i=1;i<=x;i++){                        3 e ve 4 e tam bölünen sayıların ortalaması
+            if((i%3==0)&&(i%4==0)){
+                say+=1;
+                toplam+=i;
+            }
+
+        }
+        System.out.println(toplam/say);
+
+
+
+ */
+
+
+
+    }
+}
